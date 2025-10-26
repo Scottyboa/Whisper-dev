bb71f344d02656788cb4c1c021163349d0a56dba
Scottyboa <scott_no35@hotmail.com>
2025-10-26 17:44:17 +0100

    Update promptManager.js

--- js/promptManager.js
+++ js/promptManager.js
@@ -1,17 +1,20 @@
-// promptManager.js
+// js/promptManager.js
+
 export const PromptManager = (() => {
   function hashString(str) {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
-      hash = (hash << 5) - hash + str.charCodeAt(i);
+      hash = ((hash << 5) - hash) + str.charCodeAt(i);
       hash |= 0;
     }
     return hash.toString();
   }
 
   function getPromptStorageKey(slot) {
+    // Always tie to the OpenAI key + slot number
     const apiKey = sessionStorage.getItem("openai_api_key") || "";
-    return "customPrompt_" + hashString(apiKey) + "_" + slot;
+    const hashedApiKey = hashString(apiKey);
+    return `customPrompt_${hashedApiKey}_${slot}`;
   }
 
   function loadPrompt(slot) {
@@ -20,7 +23,7 @@ export const PromptManager = (() => {
     const textarea = document.getElementById("customPrompt");
     if (textarea) {
       textarea.value = val || "";
-      textarea.dispatchEvent(new Event("input")); // auto-resize etc.
+      textarea.dispatchEvent(new Event("input"));
     }
   }
 
